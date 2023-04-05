46601eaf90fd73c7a1e9f02793aba220d0700dcb
henrikzujeddeloh <henrik.zujeddeloh@protonmail.com>
2023-04-05 14:51:43 +0200

    added comments, read file size, pass scan directory path via arguments, fixed notes per date graph

--- note-stats.py
+++ note-stats.py
@@ -1,50 +1,75 @@
 import os
-import tkinter as tk
-from tkinter import filedialog
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# Function to return creation time of file
 def get_creation_time(path):
     return os.stat(path).st_birthtime
 
-root = tk.Tk()
-root.withdraw()
 
-folder_path = filedialog.askdirectory()
+# Function to return size of file
+def get_size(path):
+    return os.stat(path).st_size
 
-df = pd.DataFrame(columns=('note', 'creation_time'))
 
+
+# Scans direcory given in excecution arguments 
+folder_path = sys.argv[1]
+
+# Creates pandas DataFrame with relevant colomns
+df = pd.DataFrame(columns=('note', 'creation_time', 'size'))
+
+# Loops through all files in selected directory
 i = 0
 for filename in os.listdir(folder_path):
+    # f is path to current file
     f = os.path.join(folder_path, filename)
+    # checks if path is a file
     if os.path.isfile(f):
         creation = pd.to_datetime(int(get_creation_time(f)), utc='True', unit='s')
-        df.loc[i] = [filename, creation]
+        size = get_size(f)
+        # adds i-th row to df with note info
+        df.loc[i] = [filename, creation, size]
         i += 1
+
+
 
 convert_tz = lambda x: x.to_pydatetime()
 get_date = lambda x: '{}-{:02}-{:02}'.format(convert_tz(x).year, convert_tz(x).month, convert_tz(x).day)
 get_month = lambda x: convert_tz(x).month
 
+# adds column to dataframe with creation date
 df['creation_date'] = df['creation_time'].map(get_date)
+# adds column to dataframe with creation month
 df['creation_month'] = df['creation_time'].map(get_month)
 print(df)
 
+
+# Sets dimensions of graph
 DPI = 100
 WIDTH = 10
 HEIGHT = 5
 
-
-creation_date = df.groupby(['creation_date']).size().rename('count')
+# creates new dataframe with number of notes created per day
+creation_date = df.groupby(['creation_date']).size()
+min_date = creation_date.index.min()
+max_date = creation_date.index.max()
+date_range = pd.date_range(min_date, max_date)
+creation_date.index = pd.DatetimeIndex(creation_date.index)
+creation_date = creation_date.reindex(date_range, fill_value=0)
 print(creation_date)
-creation_month = df.groupby(['creation_month']).size().rename('count')
+
+# creates new dataframe with number of notes created per month
+creation_month = df.groupby(['creation_month']).size()
 print(creation_month)
+
 
 plt1 = plt.subplots(figsize=(WIDTH, HEIGHT), dpi=DPI)
 
-
-plt1 = creation_date.plot(kind='line', figsize=[WIDTH,HEIGHT], linewidth=4, alpha=1, marker='o', color='b',markeredgecolor='r', markerfacecolor='w', markersize=8, markeredgewidth=2)
+plt1 = creation_date.plot(kind='line', figsize=[WIDTH,HEIGHT], linewidth=3, color='#63abdb')
 
 plt1.set_title("Note creation")
 plt1.set_xlabel("Date")
